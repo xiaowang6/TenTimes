1c7b0f71584a77d2b3a0112ebdcd3b447b86642e
laowang <819148533@qq.com>
2019-07-18 21:25:14 +0800

    初始化项目4

---
+++ src/main/java/com/itheima/pojo/a.java
@@ -0,0 +1,13 @@
+package com.itheima.pojo;
+
+/**
+ * @program: ES
+ * @author: yy
+ * @create: 2019-07-18 21:24
+ * Description: No Description
+ */
+public class a {
+    public static void main(String[] args) {
+        System.out.println("666");
+    }
+}
